9a354d14a96a8e98299f4ab12378f95ec729c255
eswar <k.eswar.92@gmail.com>
2018-07-17 18:07:22 -0400

    queue using 2 stacks

--- DataStructures/src/qUS.java
+++ DataStructures/src/qUS.java
@@ -1,15 +1,41 @@
-
+import java.util.Stack;
 public class qUS {
 
-	class queue{
-		Stack stack1;
-		Stack stack2;
+	Stack<Integer> s1 = new Stack<>();
+	Stack<Integer> s2 = new Stack<>();
+	void enqueue(int data){
+		s1.push(data);
 	}
+	void transfer(){
+		while(!s1.isEmpty()){
+			s2.push(s1.pop());
+		}
+    }
+	boolean isEmpty(){
+		return (s1.isEmpty()&&s2.isEmpty());
+	}
+	int dequeue(){
+		if(this.isEmpty()){
+			System.out.println("Queue is Empty");
+			return -1;
+		}
+		if(s2.isEmpty())
+			this.transfer();
+			return s2.pop();
+		}
+		
+	
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+    qUS q  = new qUS();
+    q.enqueue(1);
+    q.enqueue(10);
+    q.enqueue(100);
+    while(!q.isEmpty()){
+    	System.out.println(q.dequeue());
+    }
 	}
 
 }
